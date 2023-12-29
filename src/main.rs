63d2a21fa1d42261af55b9f84d1e438abca35139
n01e0 <reoshiseki@gmail.com>
2023-12-30 01:51:33 +0900

    fix dry-run option and actions

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,15 @@ mod postpone;
 
 use config::{Args, Config};
 use postpone::Postpone;
-use anyhow::Result;
+use anyhow::{Result, Context};
 use clap::Parser;
 
 #[tokio::main]
 async fn main() -> Result<()>  {
     let args = Args::parse();
-    let config = Config::new(&args)?;
-    let pattern = format!("({})", config.annotation_labels.join("|"));
 
     if args.listup {
+        let pattern = format!("({})", args.annotation_labels.with_context(|| "listup needs annotation labels")?.join("|"));
         let postpones = Postpone::search(&pattern)?;
         for postpone in postpones {
             println!("{}:{} {}\n\t{}", postpone.file, postpone.line_number, postpone.label, postpone.line);
@@ -21,9 +20,10 @@ async fn main() -> Result<()>  {
         return Ok(());
     }
     if args.dry_run {
+        let pattern = format!("({})", args.annotation_labels.with_context(|| "listup needs annotation labels")?.join("|"));
         let postpones = Postpone::search(&pattern)?
             .into_iter()
-            .map(|postpone| postpone.to_issue(&config.title_format, &config.body_format))
+            .map(|postpone| postpone.to_issue(&args.title_format.clone().with_context(|| "dry_run needs title format")?, &args.body_format.clone().with_context(|| "dry_run needs body format")?))
             .filter_map(|issue| issue.ok())
             .collect::<Vec<(String, String)>>();
 
@@ -33,6 +33,8 @@ async fn main() -> Result<()>  {
         return Ok(());
     }
 
+    let config = Config::new(&args)?;
+    let pattern = format!("({})", config.annotation_labels.join("|"));
     let client = github::GitHub::new(&config)?;
     // Created by ppb, existing Issue
     let issues = client.get_issues().await?.into_iter().filter(|issue| {
